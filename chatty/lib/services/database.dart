352bc2c6c12148d9994e938f3f9476d887e8b14a
Supatra-P <78303835+Supatra-P@users.noreply.github.com>
2022-03-26 15:00:50 +0700

    Add files via upload

---
+++ chatty/lib/services/database.dart
@@ -0,0 +1,13 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class DatabaseMethods {
+  
+  // getUserByUsername(String username) {
+
+  // }
+
+  // uploadUserInfo(String name, String email) {
+  //   // Firestore.instance.collection("user");
+  //   // .add(data)
+  // }
+}
